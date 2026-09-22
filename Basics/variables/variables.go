7aef1e86a0baef2ca15850f0ba80b0dccf130684
Love Yadav <loveyadav.8478@gmail.com>
2026-09-22 09:24:38 +0530

    Arrays impl added

--- Basics/variables/variables.go
+++ Basics/variables/variables.go
@@ -2,11 +2,31 @@ package main
 
 import "fmt"
 
+func Add(a int, b int) int{
+	return a+b
+}
+
 func main(){
 	var smsSendingLimits int
 	var costPerSms float64
 	var hasPermission bool
 	var username string
+
+	var nums [6]int
+	var dNums [] int
+	for i:= 0; i<=100; i++{
+		dNums = append(dNums, i);
+	}
+	fmt.Print(dNums)
+	println()
+	for i := 0; i<len(nums); i++{
+		nums[i] = i
+	}
+	for i := len(nums)-1; i>=0; i--{
+		fmt.Print(nums[i]," ")
+	}
+	println()
+	
 
 	fmt.Printf(
 		"%v %f %v %q\n",
@@ -15,4 +35,5 @@ func main(){
 		hasPermission,
 		username,
 	)
+	fmt.Print(Add(3,4))
 }
